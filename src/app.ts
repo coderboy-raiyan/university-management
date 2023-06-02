3ad5f6641d5a4dfcdad5b77a5bee5b816f7f844e
Raiyan <tajkierhaque@gmail.com>
2023-06-02 22:06:41 +0600

    almost done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import { Application, Request, Response } from "express";
+import { Application, Request, Response } from 'express';
 
-import cors from "cors";
-import express from "express";
+import cors from 'cors';
+import express from 'express';
 const app: Application = express();
 
 app.use(cors());
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
